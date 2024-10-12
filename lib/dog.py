8999642c2eb887d5a6b2cd62d15dea989a1eb04e
Joee <gachaujoe@gmail.com>
2024-10-12 13:02:11 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
+# class Dog:
+#     # Class body goes here
+
+#     #Instance method definition
+#     pass
+
+# lib/dog.py
 
 class Dog:
-    # Class body goes here
+    # Instance method to bark
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    # Instance method to sit
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
+# class Person:
+#     # Class body goes here
+
+#     #Instance method definition
+#     pass
+
+# lib/person.py
 
 class Person:
-    # Class body goes here
+    # Instance method to talk
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    # Instance method to walk
+    def walk(self):
+        print("The person is walking.")
